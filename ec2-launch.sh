d55d4a1861778d7b2930bd5e3afb7c113fe66485
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-02-05 11:57:21 +0530

    Update ec2-launch.sh

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -39,12 +39,11 @@ CREATE_INSTANCE() {
 }
 
 if [ "$COMPONENT" == "all" ]; then
-  for comp in frontend$ENV mongodb$ENV catalogue$ENV ; do
-    COMPONENT=$comp
+  for comp in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment dispatch ; do
+    COMPONENT=$comp$ENV
     CREATE_INSTANCE
   done
 else
   COMPONENT=$COMPONENT$ENV
   CREATE_INSTANCE
 fi
-
